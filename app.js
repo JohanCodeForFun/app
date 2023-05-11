22a2ff9660f67e78d8d7ca32a30c17b4d2fbb7e2
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-05-11 11:48:27 +0200

    add routes for pages

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.use(express.static('public'));
+const port = 3000;
+
+server.listen(port, () => {
+  console.log('http server listening on port: ' + port)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/create', (req, res) => {
+  res.sendFile(__dirname + '/create.html');
+});
+
+app.get('/update', (req, res) => {
+  res.sendFile(__dirname + '/update.html');
+});
+
+app.get('/delete', (req, res) => {
+  res.sendFile(__dirname + '/delete.html');
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
+});
+
+
